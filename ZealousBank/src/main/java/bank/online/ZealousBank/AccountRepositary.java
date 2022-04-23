8f101926f1674efee22403047f30fb80235c1106
Annamalai1995 <s.annamalai1995@gmail.com>
2022-04-23 13:43:30 +0530

    newly2

--- ZealousBank/src/main/java/bank/online/ZealousBank/AccountRepositary.java
+++ ZealousBank/src/main/java/bank/online/ZealousBank/AccountRepositary.java
@@ -1,7 +1,8 @@
 package bank.online.ZealousBank;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
+@Repository
 public interface AccountRepositary extends JpaRepository<Account, Long> {
 
 }
--- ZealousBank/src/main/java/bank/online/ZealousBank/TransactionService.java
+++ ZealousBank/src/main/java/bank/online/ZealousBank/TransactionService.java
@@ -9,5 +9,6 @@ public class TransactionService {
 	@Autowired
 	AccountRepositary repo;
 	
+	
 
 }
